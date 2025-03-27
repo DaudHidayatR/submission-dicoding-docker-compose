8043978db825992f662308309d4002b56bbec82c
Daud Hidayat Ramadahan <daud28ramadhan@gmail.com>
2025-03-28 05:24:19 +0700

    feat: comment in dockerfile, docker compose and build_push_image.sh
    Signed-off-by: Daud Hidayat Ramadahan <daud28ramadhan@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,27 @@
+#membuat dockerfile untuk image nodejs
+
+#menggunakan image nodejs versi 14 alpine
 FROM node:14-alpine
 
+#menambahkan label pada image
 LABEL authors="sagash | daud_ramadhan"
 
+#membuat folder app
 WORKDIR /app
 
+#menyalin file aplikasi ke dalam folder app
 COPY . .
 
+#menetapkan variabel environment
 ENV NODE_ENV=production DB_HOST=item-db
 
+#melakukan instalasi dependensi dan build aplikasi
 RUN npm install --production --unsafe-perm && npm run build
 
+#membuka port 8080
 EXPOSE 8080
 
+#menjalankan aplikasi dengan perintah npm start
 CMD ["npm", "start"]
 
 
--- build_push_image.sh
+++ build_push_image.sh
@@ -1,15 +1,24 @@
+#membuild image dan push ke github container registry
+
+#menginisiasi variabel
 image_app="item-app"
 image_version="v1"
 github="daudhidayatr"
 
-#docker login ghcr.io -u $github
+#melakukan login ke github container registry
 echo $CR_PAT | docker login ghcr.io -u $github --password-stdin
+
+#build image dari Dockerfile
 docker build -t ghcr.io/$github/$image_app:$image_version .
 
-docker images
-##
-docker tag item-app:v1 ghcr.io/$github/$image_app:$image_version
-##
+#menampilkan daftar image yang ada
 docker images
 
+#memberikan tag pada image yang sudah dibuild
+docker tag item-app:v1 ghcr.io/$github/$image_app:$image_version
+
+#menampilkan daftar image yang ada
+docker images
+
+#push image ke github container registry
 docker push ghcr.io/$github/$image_app:$image_version
